2365267c197487da8413d49f869956b1aada1b42
IhorKDM <ihor.sfox68@gmail.com>
2025-06-09 22:23:06 +0100

    Section 13 completed

---
+++ 13-Advanced-DOM-Bankist/script.js
@@ -0,0 +1,556 @@
+"use strict";
+
+///////////////////////////////////////
+// Modal window
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".btn--close-modal");
+const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
+const nav = document.querySelector(".nav");
+const header = document.querySelector(".header");
+
+const tabs = document.querySelectorAll(".operations__tab");
+const tabsContainer = document.querySelector(".operations__tab-container");
+const tabsContent = document.querySelectorAll(".operations__content");
+
+const openModal = function (e) {
+  e.preventDefault();
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+btnsOpenModal.forEach((btn) => btn.addEventListener("click", openModal));
+
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
+
+///////////////////////////////////////
+
+// Creating and inserting elements
+
+const message = document.createElement("div");
+message.classList.add("cookie-message");
+message.innerHTML =
+  '<p>We use cookies for improved functionality and analytics.</p> <button class="btn btn--close-cookie">Got it!</button>';
+
+header.append(message);
+
+// Deleting elements
+document
+  .querySelector(".btn--close-cookie")
+  .addEventListener("click", () => message.remove());
+
+// Styles
+message.style.backgroundColor = "#37383d";
+message.style.width = "120%";
+
+// console.log(message.style.color);
+// console.log(message.style.backgroundColor);
+// console.log(getComputedStyle(message).color);
+// console.log(getComputedStyle(message).height);
+
+message.style.height =
+  Number.parseFloat(getComputedStyle(message).height) + 30 + "px";
+
+//  ----------------------------------------------------------
+
+// Scrolling
+const btnScrollTo = document.querySelector(".btn--scroll-to");
+const section1 = document.querySelector("#section--1");
+
+btnScrollTo.addEventListener("click", function (e) {
+  const s1coords = section1.getBoundingClientRect();
+  // console.log(s1coords);
+
+  // console.log(e.target.getBoundingClientRect());
+
+  // console.log("Current scroll (X/Y)", window.pageXOffset, window.pageYOffset);
+
+  // console.log(
+  //   "Height/Width viewport",
+  //   document.documentElement.clientHeight,
+  //   document.documentElement.clientWidth
+  // );
+
+  section1.scrollIntoView({ behavior: "smooth" });
+
+  // Scrolling
+  // window.scrollTo(s1coords.left, s1coords.top + window.pageYOffset);
+
+  // window.scrollTo({
+  //   left: s1coords.left + window.pageXOffset,
+  //   top: s1coords.top + window.pageYOffset,
+  //   behavior: "smooth",
+  // });
+});
+
+/////////////////////////////////////////////////////
+// Page Navigation
+
+// document.querySelectorAll(".nav__link").forEach(function (el) {
+//   el.addEventListener("click", function (e) {
+//     e.preventDefault();
+//     const id = this.getAttribute("href");
+//     console.log(id);
+//     document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+//   });
+// });
+
+// 1. Add event listener to common parent element
+// 2. Determine what element originated the event
+
+document.querySelector(".nav__links").addEventListener("click", function (e) {
+  e.preventDefault();
+
+  // Matching strategy
+  if (e.target.classList.contains("nav__link")) {
+    const id = e.target.getAttribute("href");
+    console.log(id);
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+  }
+});
+
+///////////////////////////////////////////////////
+// Tabbed Component
+
+tabsContainer.addEventListener("click", function (e) {
+  const clicked = e.target.closest(".operations__tab");
+  console.log(clicked);
+
+  // Guard clause
+  if (!clicked) return;
+
+  // Active tab
+  tabs.forEach((t) => t.classList.remove("operations__tab--active"));
+  clicked.classList.add("operations__tab--active");
+
+  // Active content area
+  document
+    .querySelector(".operations__content--active")
+    ?.classList.remove("operations__content--active");
+  const content = document.querySelector(
+    `.operations__content--${clicked.dataset.tab}`
+  );
+  content.classList.add("operations__content--active");
+});
+
+/////////////////////////////////////////////////////
+// Menu Fade Animation
+
+const handleHover = function (e) {
+  if (e.target.classList.contains("nav__link")) {
+    const link = e.target;
+    const siblings = link.closest(".nav").querySelectorAll(".nav__link");
+    const logo = link.closest(".nav").querySelector("img");
+
+    siblings.forEach((el) => {
+      if (el !== link) el.style.opacity = this;
+    });
+    logo.style.opacity = this;
+  }
+};
+
+nav.addEventListener("mouseover", handleHover.bind(0.5));
+
+nav.addEventListener("mouseout", handleHover.bind(1));
+
+/////////////////////////////////////////////////////
+// Sticky Navigation
+// const initialCoords = section1.getBoundingClientRect();
+// console.log(initialCoords);
+
+// window.addEventListener("scroll", function (e) {
+//   console.log(this.window.scrollY);
+
+//   if (this.window.scrollY > initialCoords.top) {
+//     nav.classList.add("sticky");
+//   } else {
+//     nav.classList.remove("sticky");
+//   }
+// });
+
+// const obsCallback = function (entries, observer) {
+//   entries.forEach((entry) => {
+//     console.log(entry);
+//   });
+// };
+
+// const obsOptions = {
+//   root: null, // Use the viewport as the root
+//   threshold: [0, 0.2], // Trigger when any part of the target is visible
+// };
+
+// const observer = new IntersectionObserver(obsCallback, obsOptions);
+// observer.observe(section1);
+
+const navHeight = nav.getBoundingClientRect().height;
+
+const stickyNav = function (entries) {
+  const [entry] = entries;
+  nav.classList.toggle("sticky", !entry.isIntersecting);
+};
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
+});
+headerObserver.observe(header);
+
+/////////////////////////////////////////////////////
+// Reveal Sections
+const allSections = document.querySelectorAll(".section");
+
+const revealSection = function (entries, observer) {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) return;
+    entry.target.classList.remove("section--hidden");
+    observer.unobserve(entry.target);
+  });
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+allSections.forEach((section) => {
+  sectionObserver.observe(section);
+  section.classList.add("section--hidden");
+});
+
+/////////////////////////////////////////////////////
+// Lazy Loading Images
+const imgTargets = document.querySelectorAll("img[data-src]");
+
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
+  // console.log(entry);
+  if (!entry.isIntersecting) return;
+
+  // Replace src with data-src
+  entry.target.src = entry.target.dataset.src;
+
+  entry.target.addEventListener("load", function () {
+    entry.target.classList.remove("lazy-img");
+  });
+  observer.unobserve(entry.target);
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: "-200px",
+});
+
+imgTargets.forEach((img) => imgObserver.observe(img));
+
+/////////////////////////////////////////////////////
+// Slider
+const sliders = function () {
+  const slides = document.querySelectorAll(".slide");
+  const slider = document.querySelector(".slider");
+
+  const btnLeft = document.querySelector(".slider__btn--left");
+  const btnRight = document.querySelector(".slider__btn--right");
+  const dotContainer = document.querySelector(".dots");
+
+  let curSlide = 0;
+  const maxSlide = slides.length;
+
+  // Functions
+  const createDots = function () {
+    slides.forEach((_, i) => {
+      dotContainer.insertAdjacentHTML(
+        "beforeend",
+        `<button class="dots__dot" data-slide="${i}"></button>`
+      );
+    });
+  };
+
+  const activateDot = function (slide) {
+    document.querySelectorAll(".dots__dot").forEach((dot) => {
+      dot.classList.remove("dots__dot--active");
+    });
+
+    document
+      .querySelector(`.dots__dot[data-slide="${slide}"]`)
+      .classList.add("dots__dot--active");
+  };
+
+  const goToSlide = function (slide) {
+    slides.forEach(
+      (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
+    );
+  };
+
+  const nextSlide = function () {
+    if (curSlide === maxSlide - 1) {
+      curSlide = 0;
+    } else {
+      curSlide++;
+    }
+
+    goToSlide(curSlide);
+    activateDot(curSlide);
+  };
+
+  const prevSlide = function () {
+    if (curSlide === 0) {
+      curSlide = maxSlide - 1;
+    } else {
+      curSlide--;
+    }
+    goToSlide(curSlide);
+    activateDot(curSlide);
+  };
+
+  const init = function () {
+    goToSlide(0);
+    createDots();
+    activateDot(0);
+  };
+
+  init();
+
+  // Event handlers
+  btnRight.addEventListener("click", nextSlide);
+  btnLeft.addEventListener("click", prevSlide);
+
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") prevSlide();
+    if (e.key === "ArrowRight") nextSlide();
+  });
+
+  dotContainer.addEventListener("click", function (e) {
+    if (e.target.classList.contains("dots__dot")) {
+      curSlide = Number(e.target.dataset.slide);
+      goToSlide(curSlide);
+      activateDot(curSlide);
+    }
+  });
+};
+sliders();
+
+/////////////////////////////////////////////////////
+/////////////////////////////////////////////////////
+/////////////////////////////////////////////////////
+
+//  ----------------------------------------------------------
+// |                                                          |
+// |  Selecting, Creating, and Deleting Elements              |
+// |                                                          |
+//  ----------------------------------------------------------
+/*
+  // Selecting elements
+  console.log(document.documentElement);
+  console.log(document.head);
+  console.log(document.body);
+
+  const header = document.querySelector(".header");
+  const allSections = document.querySelectorAll(".section");
+  console.log(allSections);
+
+  document.getElementById("section--1");
+  const allButtons = document.getElementsByTagName("button");
+  console.log(allButtons);
+
+  console.log(document.getElementsByClassName("btn"));
+
+  // Creating and inserting elements
+  // .insertAdjacentHTML
+
+  const message = document.createElement("div");
+  message.classList.add("cookie-message");
+  // message.textContent = "We use cookies for improved functionality and analytics.";
+  message.innerHTML =
+    '<p>We use cookies for improved functionality and analytics.</p> <button class="btn btn--close-cookie">Got it!</button>';
+
+  // header.prepend(message);
+  header.append(message);
+  // header.append(message.cloneNode(true));
+
+  // header.before(message);
+  // header.after(message);
+
+  // Deleting elements
+  document
+    .querySelector(".btn--close-cookie")
+    .addEventListener("click", function () {
+      // message.remove();
+      message.parentElement.removeChild(message);
+    });
+*/
+
+//  ----------------------------------------------------------
+// |                                                          |
+// |  Styles, Attributes, and Classes                         |
+// |                                                          |
+//  ----------------------------------------------------------
+/*
+  console.log(message.style.height);
+
+  document.documentElement.style.setProperty("--color-primary", "orangered");
+
+  // Attributes
+  const logo = document.querySelector(".nav__logo");
+  console.log(logo.alt);
+  console.log(logo.src);
+  console.log(logo.className);
+
+  logo.alt = "Beautiful minimalist logo";
+  console.log(logo.alt);
+
+  // Non-standard
+  console.log(logo.designer);
+  console.log(logo.getAttribute("designer"));
+  logo.setAttribute("company", "Bankist");
+
+  console.log(logo.getAttribute("src"));
+
+  const link = document.querySelector(".nav__link--btn");
+  console.log(link.href);
+  console.log(link.getAttribute("href"));
+
+  // Data attributes
+  console.log(logo.dataset.versionNumber);
+
+  // Classes
+  logo.classList.add("c", "j");
+  logo.classList.remove("c", "j");
+  logo.classList.toggle("c");
+  logo.classList.contains("c");
+
+  logo.className = 'jonas'
+*/
+
+//  ----------------------------------------------------------
+// |                                                          |
+// |  Types of Events and Event Handlers                      |
+// |                                                          |
+//  ----------------------------------------------------------
+/*
+  const h1 = document.querySelector("h1");
+
+  const alertH1 = function (e) {
+    alert("Great! You are reading the heading :D");
+
+    h1.removeEventListener("mouseenter", alertH1);
+  };
+
+  h1.addEventListener("mouseenter", alertH1);
+
+  setTimeout(() => h1.removeEventListener("mouseenter", alertH1), 5000);
+
+  // h1.onmouseenter = function () {
+  //   alert("onmouseenter: Great! You are reading the heading :D");
+  // };
+*/
+
+//  ----------------------------------------------------------
+// |                                                          |
+// |  Event Propagation: Bubbling and Capturing               |
+// |                                                          |
+//  ----------------------------------------------------------
+/*
+  // rgb(255, 255, 255)
+
+  const randomInt = (min, max) =>
+    Math.floor(Math.random() * (max - min + 1) + min);
+  const randomColor = () =>
+    `rgb(${randomInt(0, 255)}, ${randomInt(0, 255)}, ${randomInt(0, 255)})`;
+  console.log(randomColor());
+
+  document.querySelector(".nav__link").addEventListener("click", function (e) {
+    this.style.backgroundColor = randomColor();
+    console.log("LINK", e.target, e.currentTarget);
+    console.log(e.target === this);
+
+    // Stop propagation
+    // e.stopPropagation();
+  });
+
+  document.querySelector(".nav__links").addEventListener("click", function (e) {
+    this.style.backgroundColor = randomColor();
+    console.log("LINKS", e.target, e.currentTarget);
+  });
+
+  document.querySelector(".nav").addEventListener(
+    "click",
+    function (e) {
+      this.style.backgroundColor = randomColor();
+      console.log("NAV", e.target, e.currentTarget);
+    },
+    false
+  );
+*/
+
+//  ----------------------------------------------------------
+// |                                                          |
+// |  DOM Traversing                                          |
+// |                                                          |
+//  ----------------------------------------------------------
+/*
+  const h1 = document.querySelector("h1");
+
+  // Going downwards: child
+  console.log(h1.querySelectorAll(".highlight"));
+  console.log(h1.childNodes);
+  console.log(h1.children);
+  h1.firstElementChild.style.color = "white";
+  h1.lastElementChild.style.color = "orangered";
+
+  // Going upwards: parent
+  console.log(h1.parentNode);
+  console.log(h1.parentElement);
+
+  h1.closest(".header").style.background = "var(--gradient-secondary)";
+
+  h1.closest("h1").style.background = "var(--gradient-primary)";
+
+  // Going sideways: siblings
+
+  console.log(h1.previousElementSibling);
+  console.log(h1.nextElementSibling);
+
+  console.log(h1.previousSibling);
+  console.log(h1.nextSibling);
+
+  console.log(h1.parentElement.children);
+
+  [...h1.parentElement.children].forEach(function (el) {
+    if (el !== h1) el.style.transform = "scale(0.5)";
+  });
+*/
+
+//  ----------------------------------------------------------
+// |                                                          |
+// |  Lifecycle DOM Events                                    |
+// |                                                          |
+//  ----------------------------------------------------------
+/*
+  document.addEventListener("DOMContentLoaded", function (e) {
+    console.log("HTML parsed and DOM tree built!", e);
+  });
+
+  window.addEventListener("load", function (e) {
+    console.log("Page fully loaded", e);
+  });
+
+  // window.addEventListener("beforeunload", function (e) {
+  //   e.preventDefault();
+  //   console.log(e);
+  //   e.returnValue = "messsage";
+  // });
+*/
--- README.md
+++ README.md
@@ -8,6 +8,8 @@ Welcome to the JavaScript Course! This guide will help you learn JavaScript from
 
 1. [Introduction](#introduction)
 
+1. [Advanced DOM Manipulation: Bankist project](#advanced-dom-manipulation-bankist-project)
+
 1. [Resources](#resources)
 
 ---
@@ -15,6 +17,30 @@ Welcome to the JavaScript Course! This guide will help you learn JavaScript from
 ## Introduction
 
 JavaScript is a versatile programming language used for web development, server-side applications, and [more](/00-Introduction/Intro.md).
+
+---
+
+## Advanced DOM Manipulation: Bankist project
+
+In this section, we will explore advanced DOM manipulation techniques using the Bankist project as an example. This project will help you understand how to create interactive web applications with JavaScript. Through this project, you will learn how to:
+
+- Select and manipulate DOM elements
+- Handle events and user interactions
+- Applying styles, atributes, and classes dynamically
+- Make smooth animations
+- Types of events and event delegation
+- Event bubbling and capturing
+- DOM traversing
+- Create components and reusable functions
+  - Slider component
+  - Modal component
+  - Tabbed component
+- The Intersection Observer API
+- Lazy loading images
+- Life cycle of a DOM element
+- Effiicient Script Loading:
+  - Defer and Async attributes
+  - Script loading order
 
 ---
 
